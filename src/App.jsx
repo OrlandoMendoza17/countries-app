6822e64bdbe94282c0a522b1d9659edd593fec8a
OrlandoMendoza17 <ommv.17@gmail.com>
2021-11-22 00:10:14 -0400

    Migración completa a webpack 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './assets/css/App.css';
-
-import Header from './components/Header';
-import Home from './pages/Home';
-
-import Details from './pages/Details';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Details from "./pages/Details"
 
 const App = () => {
   return (
     <div className={`App ${""}`}>
-      <Header toggleDarkMode={()=>{}} darkModeOn={false} />
-      <Router>
-        <Switch> {/* The Switch decides which component to show based on the current URL.*/}
+      {/* <Header toggleDarkMode={()=>{}} darkModeOn={false} /> */}
+      <BrowserRouter>
+        <Routes> 
+          The Routes decides which component to show based on the current URL.
 
-          <Route exact path='/' component={Home} />
+          <Route exact path='/' element={<Home/>} />
 
-          <Route exact path='/country/:name' component={Details} />
-          <Route exact path='/alpha/:name' component={Details} />
+          <Route exact path='/country/:name' element={<Details/>} />
+          <Route exact path='/alpha/:name' element={<Details/>} />
 
-        </Switch>
-      </Router>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
